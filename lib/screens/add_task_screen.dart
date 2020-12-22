6405cd2eef19254b9e0230a5fe87ccdd84163979
Your Name <hossamomar9876@gmail.com>
2020-12-22 14:42:08 +0200

    finish ui of app

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -17,6 +17,18 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   final DateFormat _dateFormat = DateFormat('MMM dd, yyyy');
   final List<String> _levels = ['Low', 'Medium', 'High'];
 
+  @override
+  void initState() {
+    super.initState();
+    _dateController.text = _dateFormat.format(_date);
+  }
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    super.dispose();
+  }
+
   _handleDatePicker() async {
     print('showDatePicker');
     final DateTime date = await showDatePicker(
@@ -33,116 +45,159 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
     _dateController.text = _dateFormat.format(_date);
   }
 
+  _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print('title: $_title, date: $_date, level: $_priority');
+      // Insert DB
+
+      // Update DB
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 80.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    color: Color(0xFFE26253),
-                    size: 30.0,
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 80.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Color(0xFFE26253),
+                      size: 30.0,
+                    ),
                   ),
-                ),
-                SizedBox(height: 20.0),
-                Text(
-                  'Add Task',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Add Task',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 10.0),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.0),
-                        child: TextFormField(
-                          style: TextStyle(
-                            fontSize: 18.0,
-                          ),
-                          decoration: InputDecoration(
-                            labelText: 'Title',
-                            labelStyle: TextStyle(fontSize: 18.0),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+                  SizedBox(height: 10.0),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: TextFormField(
+                            style: TextStyle(
+                              fontSize: 18.0,
                             ),
-                          ),
-                          validator: (input) => input.trim().toString().isEmpty
-                              ? 'Please enter a task title'
-                              : null,
-                          onSaved: (value) => _title = value,
-                          initialValue: _title,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.0),
-                        child: TextFormField(
-                          readOnly: true,
-                          controller: _dateController,
-                          onTap: _handleDatePicker,
-                          style: TextStyle(fontSize: 18.0),
-                          decoration: InputDecoration(
-                            labelText: 'Date',
-                            labelStyle: TextStyle(fontSize: 18.0),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.0),
-                        child: DropdownButtonFormField(
-                          icon: Icon(Icons.arrow_drop_down_circle_outlined),
-                          iconSize: 22.0,
-                          iconEnabledColor: Color(0xFFE26253),
-                          items: _levels.map((String level) {
-                            return DropdownMenuItem(
-                              child: Text(
-                                level,
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  color: Colors.black,
-                                ),
+                            decoration: InputDecoration(
+                              labelText: 'Title',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
                               ),
-                              value: level,
-                            );
-                          }).toList(),
-                          style: TextStyle(
-                            fontSize: 18.0,
+                            ),
+                            validator: (input) =>
+                                input.trim().toString().isEmpty
+                                    ? 'Please enter a task title'
+                                    : null,
+                            onSaved: (value) => _title = value,
+                            initialValue: _title,
                           ),
-                          decoration: InputDecoration(
-                            labelText: 'Priority',
-                            labelStyle: TextStyle(fontSize: 18.0),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: TextFormField(
+                            readOnly: true,
+                            controller: _dateController,
+                            onTap: _handleDatePicker,
+                            style: TextStyle(fontSize: 18.0),
+                            decoration: InputDecoration(
+                              labelText: 'Date',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
                             ),
                           ),
-                          validator: (input) => _priority == null
-                              ? 'Please select a piority level'
-                              : null,
-                          onChanged: (value) {
-                            setState(() {
-                              _priority = value;
-                            });
-                          },
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: DropdownButtonFormField(
+                            icon: Icon(Icons.arrow_drop_down_circle_outlined),
+                            iconSize: 22.0,
+                            iconEnabledColor: Color(0xFFE26253),
+                            items: _levels.map((String level) {
+                              return DropdownMenuItem(
+                                child: Text(
+                                  level,
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                value: level,
+                              );
+                            }).toList(),
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                            decoration: InputDecoration(
+                              labelText: 'Priority',
+                              labelStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            validator: (input) => _priority == null
+                                ? 'Please select a piority level'
+                                : null,
+                            onChanged: (value) {
+                              setState(() {
+                                _priority = value;
+                              });
+                            },
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 60.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30.0),
+                            color: Color(0xffe74c3c),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey[500].withOpacity(0.4),
+                                spreadRadius: 3,
+                                blurRadius: 7,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          margin: EdgeInsets.symmetric(vertical: 20.0),
+                          child: FlatButton(
+                            onPressed: _submit,
+                            child: Text(
+                              'Add',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),

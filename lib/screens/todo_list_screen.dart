d3abcf23e3aa9490dd97381941a7e2aab9cd7a93
Your Name <hossamomar9876@gmail.com>
2020-12-24 16:10:25 +0200

    update project

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -1,3 +1,5 @@
+import 'package:Todo_List_App_Flutter/helpers/database_helper.dart';
+import 'package:Todo_List_App_Flutter/models/task_model.dart';
 import 'package:Todo_List_App_Flutter/screens/add_task_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,64 +9,86 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
+  Future<List<TaskModel>> _taskList;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTaskList();
+  }
+
+  _updateTaskList() {
+    setState(() {
+      _taskList = DatabaseHelper.instance.getTaskList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.symmetric(vertical: 50.0),
-          child: ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              if (index == 0) {
+        body: FutureBuilder(
+          future: _taskList,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              padding: EdgeInsets.symmetric(vertical: 50.0),
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'My Tasks',
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff2c3e50),
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          '3 of 6',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Color(0xFFA6ABAD),
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
                 return Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'My Tasks',
-                        style: TextStyle(
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xff2c3e50),
+                      ListTile(
+                        title: Text('Title'),
+                        subtitle: Text('Subtitle'),
+                        trailing: Checkbox(
+                          onChanged: (value) {
+                            print(value);
+                          },
+                          activeColor: Color(0xFFE26253),
+                          value: true,
                         ),
                       ),
-                      SizedBox(height: 10.0),
-                      Text(
-                        '3 of 6',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Color(0xFFA6ABAD),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
+                      Divider(),
                     ],
                   ),
                 );
-              }
-              return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: Column(
-                  children: [
-                    ListTile(
-                      title: Text('Title'),
-                      subtitle: Text('Subtitle'),
-                      trailing: Checkbox(
-                        onChanged: (value) {
-                          print(value);
-                        },
-                        activeColor: Color(0xFFE26253),
-                        value: true,
-                      ),
-                    ),
-                    Divider(),
-                  ],
-                ),
-              );
-            },
-            itemCount: 10,
-          ),
+              },
+              itemCount: 1 + snapshot.data.length,
+            );
+          },
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => Navigator.push(
